dd7e7468985a015a91a2fba4a120548c6474a5a7
Nathan Leiby <nathanleiby@gmail.com>
2022-02-27 13:44:46 -0800

    test scripts -- better logs, moar colors

--- examples.sh
+++ examples.sh
@@ -1,7 +1,10 @@
 set -e
+
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+
 for x in `ls examples/*.lox`; do
-    echo "=== TEST: $x ==="
+    echo "${CYAN}=== TEST: $x ===${NC}"
     zig run main.zig -- $x
     echo ""
 done;
-
--- test.sh
+++ test.sh
@@ -1,1 +1,10 @@
-ls *.zig | xargs -L 1 zig test
+set -e
+
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+
+for x in `ls *.zig`; do
+    echo "${CYAN}=== TEST: $x ===${NC}"
+    zig test $x
+    echo ""
+done;
