f9c03a2a743a863a2e37e8a5c1f37aac8c95cc80
Nathan Leiby <nathanleiby@gmail.com>
2022-02-27 11:19:30 -0800

    disable debugging by default

--- debug.zig
+++ debug.zig
@@ -1,9 +1,9 @@
 // Debug Flags
 
 // VM (Runtime)
-pub const TRACE_EXECUTION_INCLUDE_INSTRUCTIONS = true;
+pub const TRACE_EXECUTION_INCLUDE_INSTRUCTIONS = false;
 pub const TRACE_EXECUTION_SHOW_GET_SET_VARS = false;
-pub const TRACE_EXECUTION_PRINT_STACK = true;
+pub const TRACE_EXECUTION_PRINT_STACK = false;
 
 // Compiler
 pub const PRINT_CODE_AFTER_END_COMPILER = false; // TODO: try out comptime
