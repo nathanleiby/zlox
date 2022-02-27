671e050823a10b3a040b05e701e5f54236f3ddd2
Nathan Leiby <nathanleiby@gmail.com>
2022-02-27 13:37:41 -0800

    tweak debug flags

--- debug.zig
+++ debug.zig
@@ -1,10 +1,13 @@
 // Debug Flags
 
+// global flag to enable/disable all
+const enable_debugging = false;
+
 // VM (Runtime)
-pub const TRACE_EXECUTION_INCLUDE_INSTRUCTIONS = false;
-pub const TRACE_EXECUTION_SHOW_GET_SET_VARS = false;
-pub const TRACE_EXECUTION_PRINT_STACK = false;
+pub const TRACE_EXECUTION_INCLUDE_INSTRUCTIONS = enable_debugging and false;
+pub const TRACE_EXECUTION_SHOW_GET_SET_VARS = enable_debugging and false;
+pub const TRACE_EXECUTION_PRINT_STACK = enable_debugging and false;
 
 // Compiler
-pub const PRINT_CODE_AFTER_END_COMPILER = false; // TODO: try out comptime
-pub const TRACE_PARSER = false;
+pub const PRINT_CODE_AFTER_END_COMPILER = enable_debugging and false; // TODO: try out comptime
+pub const TRACE_PARSER = enable_debugging and false;
